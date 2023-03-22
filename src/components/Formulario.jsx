ae9ac0c83ebd21a5c54dbbc5c8addef2bad671d4
Nerovelic <zack.vadhir2@hotmail.com>
2023-03-21 22:10:23 -0700

    ahora muestra rueda de recarga cuando la pagina esta lenta

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -7,21 +7,26 @@ import Error from "./Error";
 const InputSubmit = styled.input`
     background-color:#6061c9;
     border: none;
-    margin: 100;
+    margin: 20px 0;
     padding: 10px;
     color: white;
     font-weight: 900;
     text-transform: uppercase;
     font-size: 20px;
     border-radius: 12px;
-    margin-left: 110px;
-    width: 230px;
-    height: 50px;
+    max-width: 100%;
+    width: 100%;
     transition: background-color .3s ease;
 
     &:hover{
         background-color: #fdfdfd6a;
         cursor: pointer;
+    }
+
+    @media (min-width: 992px) {  
+      max-width: 50%;
+      width: 50%;
+      margin: 20px auto;
     }
 `;
 
@@ -33,9 +38,36 @@ const Resultado = styled.p`
 `;
 
 const Imagenes = styled.img`
-    margin: 10px auto;
+    display: block;
+    margin: 20px 0;
     max-width: 100px;
-    margin-left: 150px;
+
+    @media (max-width: 992px) {
+    form {
+       display: flex;
+       flex-direction: column;
+       align-items: center;
+     }
+    }
+`;
+
+const Cargando = styled.div`
+  border: 5px solid #f3f3f3;
+  border-top: 5px solid #6061c9;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  margin: 30px auto;
+  animation: spin 1s linear infinite;
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 `;
 
 const ResultadoContainer = styled.div`
@@ -45,6 +77,7 @@ const ResultadoContainer = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    text-align: center;
     padding: 20px;
     margin-top: 50px;
 `;
@@ -56,11 +89,15 @@ const Formulario = () =>{
     const [crypt,SelectorCryptos] = useSelectorMonedas('Elige tu cryptomoneda:',cryptos);
     const [resultadoConsulta, setResultadoConsulta] = useState({});
     const [valorCotizado, setValorCotizado] = useState(null);
+    const [valorAlto, setValorAlto] = useState(null);
+    const [valorBajo, setValorBajo] = useState(null);
     const [imagen, setImagen] = useState('');
+    const [mostrarCargando, setMostrarCargando] = useState(false);
 
     //conslutar una api//
     useEffect(() =>{
         const consultarApi = async () =>{
+            setMostrarCargando(true);
             const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD";
             const respuesta = await fetch(url);
             const resultado = await respuesta.json();
@@ -73,10 +110,11 @@ const Formulario = () =>{
                 return objeto;
             });
             setCryptos(arregloCryptos);
+            setMostrarCargando(false);
         }
         consultarApi();
     }, [])
-
+      
     const manejadorSubmit = async e => {
         e.preventDefault();
         if([state,crypt].includes('')){
@@ -85,12 +123,16 @@ const Formulario = () =>{
             return;
         }
         setError(false);
+        setMostrarCargando(true);
         const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${crypt}&tsyms=${state}`;
         const respuesta = await fetch(url);
         const resultado = await respuesta.json();
         setResultadoConsulta(resultado);
         setValorCotizado(resultado.DISPLAY[crypt][state].PRICE);
+        setValorAlto(resultado.DISPLAY[crypt][state].HIGHDAY);
+        setValorBajo(resultado.DISPLAY[crypt][state].LOWDAY);
         setImagen(resultado.RAW[crypt][state].IMAGEURL);
+        setMostrarCargando(false);
     }
 
     return(
@@ -104,10 +146,17 @@ const Formulario = () =>{
             type="submit" 
             value="Cotizar"/>
         </form>
+        {mostrarCargando && <Cargando></Cargando>}
         {valorCotizado && (
             <ResultadoContainer>
-                <Imagenes src={`https://www.cryptocompare.com/${imagen}`} alt="Imagen de la crypto" />
-                <Resultado>El valor de {crypt} en {state} es de: {valorCotizado}</Resultado>
+                <Resultado>{crypt}</Resultado>
+                <Imagenes 
+                    src={`https://www.cryptocompare.com/${imagen}`} 
+                    alt="Imagen de la crypto" 
+                />
+                <Resultado>El valor es de: <span>{valorCotizado}</span> </Resultado>
+                <Resultado>Su valor mas alto es de: <span>{valorAlto}</span></Resultado>
+                <Resultado>Su valor mas bajo es de: <span>{valorBajo}</span></Resultado>
             </ResultadoContainer>
         )}
         </div>
